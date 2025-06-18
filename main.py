b30a3ab8f39a7ef897d9913810153f4d47c55ba3
Cory Plastek <cory@coryplastek.com>
2025-06-17 20:55:06 -0500

    MOAR STATS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_stats
 
 def get_book_text(path):
     with open(path) as f:
@@ -6,8 +6,12 @@ def get_book_text(path):
         return file_contents
 
 def main():
-    num = get_num_words(get_book_text('books/frankenstein.txt'))
+    text = get_book_text('books/frankenstein.txt')
+    num = get_num_words(text)
     message = f"{num} words found in the document"
     print(message)
+
+    stats = get_character_stats(text)
+    print(stats)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(text):
     return len(text.split())
 
+def get_character_stats(text):
+    stats = {}
+    for c in list(text):
+        c = c.lower()
+        if c in stats:
+            stats[c] = stats[c] + 1
+        else:
+            stats[c] = 1
+    return stats
